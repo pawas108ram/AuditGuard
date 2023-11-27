09a325b98110d30bede2dd67817117beacc67761
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2023-11-27 16:12:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,15 @@ const App = () => {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/services' component={Services} />
-          <Route path='/faqs' component={FAQs} />
-        </Switch>
+        <div>
+          <Navbar/>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/services' component={Services} />
+            <Route path='/faqs' component={FAQs} />
+          </Switch>
+        </div>
       </Router>
     </div>
   )
